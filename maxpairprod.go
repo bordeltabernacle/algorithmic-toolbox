d0b5a190a762fe5b84b11284e91b742a797097ea
Rob Phoenix <robphoenix@protonmail.com>
2016-12-28 14:48:54 +0000

    add max pairwise product solution

---
+++ maxpairprod.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(MaxPairwiseProduct([]int{4, 6, 2, 6, 1}))
+}
+
+// MaxPairwiseProduct finds the maximum pairwise product given a sequence of
+// non-negative integers, that is, the largest integer that can be obtained by
+// multiplying two different elements from the sequence.
+func MaxPairwiseProduct(seq []int) int {
+	tmp := make([]int, len(seq))
+	copy(tmp, seq)
+	index1 := maxElementIndex(tmp)
+	max1 := tmp[index1]
+	tmp = append(tmp[:index1], tmp[index1+1:]...)
+	index2 := maxElementIndex(tmp)
+	return max1 * tmp[index2]
+}
+
+func maxElementIndex(seq []int) (max int) {
+	for i, v := range seq {
+		if v > seq[max] {
+			max = i
+		}
+	}
+	return
+}
---
+++ maxpairprod_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+type testCase struct {
+	input    []int
+	expected int
+}
+
+var testCases = []testCase{
+	{[]int{1, 2, 3}, 6},
+	{[]int{7, 5, 14, 2, 8, 8, 10, 1, 2, 3}, 140},
+	{[]int{4, 6, 2, 6, 1}, 36},
+	{[]int{100000, 90000}, 9000000000},
+}
+
+func TestMaxPairProd(t *testing.T) {
+	for _, test := range testCases {
+		actual := MaxPairwiseProduct(test.input)
+		if actual != test.expected {
+			t.Errorf("Max Pairwise Product test with %v\n\texpected: %d\n\t  actual: %d", test.input, test.expected, actual)
+		}
+	}
+
+}
